88ef65303545b29367f9918cabe08b52d4cc7764
Simon Morgan <sjm@sjm.io>
2022-07-08 18:06:31 +0100

    Implement is_valid

--- src/chunk_type.rs
+++ src/chunk_type.rs
@@ -54,7 +54,7 @@ impl ChunkType {
         self.0
     }
     fn is_valid(&self) -> bool {
-        todo!()
+        self.0.iter().all(|b| b.is_ascii_alphabetic()) && self.is_reserved_bit_valid()
     }
     fn is_critical(&self) -> bool {
         todo!()

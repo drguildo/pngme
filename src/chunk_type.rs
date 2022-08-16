b6c1fce42e3b92d26624a365fa665358be0a5be3
Simon Morgan <sjm@sjm.io>
2022-08-16 18:22:56 +0100

    Convert error type instead of calling unwrap()

--- src/chunk_type.rs
+++ src/chunk_type.rs
@@ -34,7 +34,8 @@ impl FromStr for ChunkType {
 
 impl Display for ChunkType {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        std::fmt::Display::fmt(&std::str::from_utf8(&self.0).unwrap(), f)
+        let s = std::str::from_utf8(&self.0).map_err(|_e| std::fmt::Error)?;
+        std::fmt::Display::fmt(s, f)
     }
 }
 
